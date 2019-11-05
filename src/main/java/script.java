dd94edb7b3e11314c5616400d6b273246ef20b3d
Gaurab Dasgupta <gaurabdasg@gmail.com>
2019-11-05 20:14:24 +0530

    added new station check

--- src/main/java/script.java
+++ src/main/java/script.java
@@ -1,12 +1,10 @@
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -118,21 +116,48 @@ public class script {
         driver.navigate().to("http://psd.bits-pilani.ac.in/Student/ViewActiveStationProblemBankData.aspx");
 
         ArrayList<Station> data = new ArrayList<>();
+        HashSet<Integer> companyIds = new HashSet<>();
 
         WebElement tab = driver.findElement(By.xpath("//table[@id='data-table-hrteam']//tbody"));
         List<WebElement> rows = tab.findElements(By.tagName("tr"));
+        ArrayList<Station> newps = new ArrayList<>();
+        HashSet prev = null;
+        boolean firstRun = false;
+        if(!firstRun)
+        {
+            try(ObjectInputStream fin = new ObjectInputStream(new FileInputStream("./data/prevData.ser")))
+            {
+                prev = (HashSet)fin.readObject();
+            }
+            catch(Exception e){}
+        }
 
         for(WebElement row:rows)
         {
             List<WebElement> cols = row.findElements(By.tagName("td"));
-            data.add(new Station(cols.get(2).getText(),
+            Station t = new Station(cols.get(2).getText(),
                     cols.get(3).getText(),
                     cols.get(1).getText(),
                     cols.get(5).getAttribute("stationid"),
                     cols.get(5).getAttribute("companyid")
-            ));
+            );
+            data.add(t);
+            if(!firstRun && !prev.contains(Integer.parseInt(cols.get(5).getAttribute("companyid"))))
+                newps.add(t);
+            companyIds.add(Integer.parseInt(cols.get(5).getAttribute("companyid")));
         }
+        if(prev.size()!=companyIds.size())
+            System.out.println(Math.abs(prev.size() - companyIds.size()) +" New Stations Added!");
 
+        // serialize hashset
+        try(ObjectOutputStream fout = new ObjectOutputStream(new FileOutputStream("./data/prevData.ser")))
+        {
+            fout.writeObject(companyIds);
+        }catch(Exception e){}
+
+//        assert statIds.size()==companyIds.size() : "unequal";
+
+        // scrape stipend and branches
         for(Station s:data)
         {
             driver.navigate().to("http://psd.bits-pilani.ac.in/Student/StationproblemBankDetails.aspx?CompanyId="+s.getCompanyId()+"&StationId="+s.getStationId()+"&BatchIdFor=9&PSTypeFor=3");
@@ -144,30 +169,37 @@ public class script {
                 br = driver.findElement(By.xpath(("//*[@id=\"Project\"]/table/tbody/tr[5]/td[3]/div")));
                 des = driver.findElement(By.xpath("/html/body/form/div[3]/div[2]/div/div[2]/table[1]/tbody/div/table/tbody/tr[2]/td[2]"));
             }catch(Exception e)
-            {
-//                e.printStackTrace();
-            }
+            {}
             s.setStipend(stp==null?"":stp.getText());
             s.setBranches(br==null?"":br.getText());
-            s.setDescription(des==null?"":des.getText());
+            String clean = "";
+            try{
+                clean = des.getText();
+            }
+            catch (Exception e)
+            {}
+            clean = clean==null?"":clean.replace(',', '|');
+            s.setDescription(clean);
         }
+
+        if(!newps.isEmpty())
+            for(Station s:newps)
+                System.out.println(s.getName()+" "+s.getLocation()+" "+s.getStipend()+" "+s.getDomain());
 
         // write to csv
         File file = new File("./data/stations_"+new SimpleDateFormat("dd-MM-yyyy").format(new Date())+".csv");
 
         try(BufferedWriter bw = new BufferedWriter(new FileWriter(file));){
-            bw.write("Name,Domain,Stipend,Location,Branches");
+            bw.write("Name,Domain,Stipend,Location,Branches,Project Description");
             bw.newLine();
             for(Station st:data)
             {
                 bw.write(st.getName()+","+st.getDomain()+","+st.getStipend()+","+st.getLocation()+","+st.getBranches()+","+st.getDescription());
                 bw.newLine();
             }
-        }catch(IOException e)
-        {
-            e.printStackTrace();
-        }
+        }catch(IOException e) {}
 
         driver.close();
     }
 }
+// 243
