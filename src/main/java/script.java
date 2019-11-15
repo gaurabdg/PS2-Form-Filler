bd2a5a9dab043a2f5e36ba86b39a92c968b93126
Gaurab Dasgupta <gaurabdasg@gmail.com>
2019-11-16 03:48:04 +0530

    optimized form filling

--- src/main/java/script.java
+++ src/main/java/script.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.interactions.Actions;
 import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.*;
-import java.util.concurrent.TimeUnit;
 
 class Station{
     private String name;
@@ -103,124 +102,114 @@ class Station{
 }
 
 public class script {
-    public static void main(String[] args) throws IOException {
-        System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver");
-        WebDriver driver = new ChromeDriver();
+    private static void fetchData(WebDriver driver)
+    {
+        driver.navigate().to("http://psd.bits-pilani.ac.in/Student/ViewActiveStationProblemBankData.aspx");
 
-        // login
-        driver.navigate().to("http://psd.bits-pilani.ac.in");
-        WebElement username = driver.findElement(By.id("TxtEmail"));
-        username.clear();
-        username.sendKeys("f20160255@hyderabad.bits-pilani.ac.in");
-        WebElement pass = driver.findElement(By.id("txtPass"));
-        pass.clear();
-        pass.sendKeys("4V113RHO");
-        driver.findElement(By.id("Button1")).click();
+        ArrayList<Station> data = new ArrayList<>();
+        HashSet<Integer> companyIds = new HashSet<>();
 
-        // scrape table
-//        driver.navigate().to("http://psd.bits-pilani.ac.in/Student/ViewActiveStationProblemBankData.aspx");
-//
-//        ArrayList<Station> data = new ArrayList<>();
-//        HashSet<Integer> companyIds = new HashSet<>();
-//
-//        WebElement tab = driver.findElement(By.xpath("//table[@id='data-table-hrteam']//tbody"));
-//        List<WebElement> rows = tab.findElements(By.tagName("tr"));
-//        ArrayList<Station> newps = new ArrayList<>();
-//        HashSet prev = null;
-//
-//        try(ObjectInputStream fin = new ObjectInputStream(new FileInputStream("./data/prevData.ser")))
-//        {
-//            prev = (HashSet)fin.readObject();
-//        }
-//        catch(Exception e){}
-//
-//        for(WebElement row:rows)
-//        {
-//            List<WebElement> cols = row.findElements(By.tagName("td"));
-//            Station t = new Station(cols.get(2).getText(),
-//                    cols.get(3).getText(),
-//                    cols.get(1).getText(),
-//                    cols.get(5).getAttribute("stationid"),
-//                    cols.get(5).getAttribute("companyid")
-//            );
-//
-//            data.add(t);
-//
-//            if(!prev.contains(Integer.parseInt(cols.get(5).getAttribute("companyid"))))
-//                newps.add(t);
-//            companyIds.add(Integer.parseInt(cols.get(5).getAttribute("companyid")));
-//        }
-//
-//        if(prev.size()!=companyIds.size())
-//            System.out.println(Math.abs(prev.size() - companyIds.size()) +" New Stations Added!");
-//
-//        // store meta
-//        try(BufferedWriter bw = new BufferedWriter(new FileWriter("./data/meta.txt")))
-//        {
-//            bw.write(Integer.toString(companyIds.size()));
-//
-//        }catch(Exception e){}
-//
-//        // serialize HashSet
-//        try(ObjectOutputStream fout = new ObjectOutputStream(new FileOutputStream("./data/prevData.ser")))
-//        {
-//            fout.writeObject(companyIds);
-//        }catch(Exception e){}
-//
-//        if(!newps.isEmpty())
-//            for(Station s:newps)
-//                System.out.println(s.getName()+" || "+s.getLocation()+" || "+s.getDomain());
-//
-//        for(Station s:data)
-//        {
-//            System.out.print("Fetching details of station: "+" "+s.getStationId()+" "+s.getCompanyId()+" "+s.getName());
-//            driver.navigate().to("http://psd.bits-pilani.ac.in/Student/StationproblemBankDetails.aspx?CompanyId="+s.getCompanyId()+"&StationId="+s.getStationId()+"&BatchIdFor=9&PSTypeFor=3");
-//            WebElement stp = null;
-//            WebElement br = null;
-//            WebElement des = null;
-//            WebElement skillSet = null;
-//            WebElement prefElec = null;
-//            try {
-//                stp = driver.findElement(By.xpath("//*[@id=\"Stipend\"]"));
-//                br = driver.findElement(By.xpath(("//*[@id=\"Project\"]/table/tbody/tr[5]/td[3]/div")));
-//                des = driver.findElement(By.xpath("/html/body/form/div[3]/div[2]/div/div[2]/table[1]/tbody/div/table/tbody/tr[2]/td[2]"));
-//                skillSet = driver.findElement(By.xpath("/html/body/form/div[3]/div[2]/div/div[2]/table[1]/tbody/div/table/tbody/tr[3]/td[2]"));
-//                prefElec = driver.findElement(By.xpath("/html/body/form/div[3]/div[2]/div/div[2]/table[1]/tbody/div/table/tbody/tr[5]/td[2]"));
-//
-//            }catch(Exception e)
-//            {}
-//            s.setStipend(stp==null?"":stp.getText());
-//            s.setBranches(br==null?"":br.getText());
-//            s.setSkillSet(skillSet==null?"":skillSet.getText());//==null?"":skillSet.getText()));
-//            s.setPrefElecs(prefElec==null?"":prefElec.getText());
-//            String clean = "";
-//            try{
-//                assert des != null;
-//                clean = des.getText();
-//            }
-//            catch (NullPointerException e)
-//            {}
-//            clean = clean==null?"":clean.replace(',', '|');
-//            s.setDescription(clean);
-//            System.out.println(" || "+s.getStipend()+" || "+s.getBranches());
-//        }
-//
-//        // write to csv
-//        File file = new File("./data/stations_"+new SimpleDateFormat("dd-MM-yyyy").format(new Date())+".csv");
-//
-//        try(BufferedWriter bw = new BufferedWriter(new FileWriter(file))){
-//            bw.write("StationId,Name,Domain,Stipend,Location,Branches,Project Description,Reqd Skill Sets, Reqd Electives");
-//            bw.newLine();
-//            for(Station st:data)
-//            {
-//                bw.write(st.getStationId()+","+st.getName()+","+st.getDomain()+","+st.getStipend()+","+st.getLocation()+","+st.getBranches()+","+st.getDescription()+","+st.getSkillSet()+","+st.getPrefElecs());
-//                bw.newLine();
-//            }
-//        }catch(IOException e){}
+        WebElement tab = driver.findElement(By.xpath("//table[@id='data-table-hrteam']//tbody"));
+        List<WebElement> rows = tab.findElements(By.tagName("tr"));
+        ArrayList<Station> newps = new ArrayList<>();
+        HashSet prev = null;
 
-        // PART 2 - csv to pref fill
+        try(ObjectInputStream fin = new ObjectInputStream(new FileInputStream("./data/prevData.ser")))
+        {
+            prev = (HashSet)fin.readObject();
+        }
+        catch(Exception e){}
+
+        for(WebElement row:rows)
+        {
+            List<WebElement> cols = row.findElements(By.tagName("td"));
+            Station t = new Station(cols.get(2).getText(),
+                    cols.get(3).getText(),
+                    cols.get(1).getText(),
+                    cols.get(5).getAttribute("stationid"),
+                    cols.get(5).getAttribute("companyid")
+            );
+
+            data.add(t);
+
+            if(!prev.contains(Integer.parseInt(cols.get(5).getAttribute("companyid"))))
+                newps.add(t);
+            companyIds.add(Integer.parseInt(cols.get(5).getAttribute("companyid")));
+        }
+
+        if(prev.size()!=companyIds.size())
+            System.out.println(Math.abs(prev.size() - companyIds.size()) +" New Stations Added!");
+
+        // store meta
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter("./data/meta.txt")))
+        {
+            bw.write(Integer.toString(companyIds.size()));
+
+        }catch(Exception e){}
+
+        // serialize HashSet
+        try(ObjectOutputStream fout = new ObjectOutputStream(new FileOutputStream("./data/prevData.ser")))
+        {
+            fout.writeObject(companyIds);
+        }catch(Exception e){}
+
+        if(!newps.isEmpty())
+            for(Station s:newps)
+                System.out.println(s.getName()+" || "+s.getLocation()+" || "+s.getDomain());
+
+        for(Station s:data)
+        {
+            System.out.print("Fetching details of station: "+" "+s.getStationId()+" "+s.getCompanyId()+" "+s.getName());
+            driver.navigate().to("http://psd.bits-pilani.ac.in/Student/StationproblemBankDetails.aspx?CompanyId="+s.getCompanyId()+"&StationId="+s.getStationId()+"&BatchIdFor=9&PSTypeFor=3");
+            WebElement stp = null;
+            WebElement br = null;
+            WebElement des = null;
+            WebElement skillSet = null;
+            WebElement prefElec = null;
+            try {
+                stp = driver.findElement(By.xpath("//*[@id=\"Stipend\"]"));
+                br = driver.findElement(By.xpath(("//*[@id=\"Project\"]/table/tbody/tr[5]/td[3]/div")));
+                des = driver.findElement(By.xpath("/html/body/form/div[3]/div[2]/div/div[2]/table[1]/tbody/div/table/tbody/tr[2]/td[2]"));
+                skillSet = driver.findElement(By.xpath("/html/body/form/div[3]/div[2]/div/div[2]/table[1]/tbody/div/table/tbody/tr[3]/td[2]"));
+                prefElec = driver.findElement(By.xpath("/html/body/form/div[3]/div[2]/div/div[2]/table[1]/tbody/div/table/tbody/tr[5]/td[2]"));
+
+            }catch(Exception e)
+            {}
+            s.setStipend(stp==null?"":stp.getText());
+            s.setBranches(br==null?"":br.getText());
+            s.setSkillSet(skillSet==null?"":skillSet.getText());//==null?"":skillSet.getText()));
+            s.setPrefElecs(prefElec==null?"":prefElec.getText());
+            String clean = "";
+            try{
+                assert des != null;
+                clean = des.getText();
+            }
+            catch (NullPointerException e)
+            {}
+            clean = clean==null?"":clean.replace(',', '|');
+            s.setDescription(clean);
+            System.out.println(" || "+s.getStipend()+" || "+s.getBranches());
+        }
+
+        // write to csv
+        File file = new File("./data/stations_"+new SimpleDateFormat("dd-MM-yyyy").format(new Date())+".csv");
+
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(file))){
+            bw.write("StationId,Name,Domain,Stipend,Location,Branches,Project Description,Reqd Skill Sets, Reqd Electives");
+            bw.newLine();
+            for(Station st:data)
+            {
+                bw.write(st.getStationId()+","+st.getName()+","+st.getDomain()+","+st.getStipend()+","+st.getLocation()+","+st.getBranches()+","+st.getDescription()+","+st.getSkillSet()+","+st.getPrefElecs());
+                bw.newLine();
+            }
+        }catch(IOException e){}
+    }
+
+    private static void uploadData(WebDriver driver, String path) throws IOException {
         driver.navigate().to("http://psd.bits-pilani.ac.in/Student/StudentStationPreference.aspx");
-        HashMap<String, Integer> sidToPos = new HashMap<String, Integer>();
+        HashMap<String, Integer> sidToPos = new HashMap<>();
+        // for testing
+//        driver.findElement(By.xpath("//*[@id=\"Reset\"]")).click();
 
         WebElement statList = driver.findElement(By.xpath("//*[@id=\"sortable_nav\"]"));
         List<WebElement> stats = statList.findElements(By.tagName("li"));
@@ -230,14 +219,13 @@ public class script {
             WebElement curr = driver.findElement(By.xpath("//*[@id=\"sortable_nav\"]/li["+i+"]/span"));
             sidToPos.put(curr.getAttribute("spn"),i);
         }
-//        for(Map.Entry e : sidToPos.entrySet())
-//            System.out.println(e.getKey()+" "+e.getValue());
+
         System.out.println("starting swaps...");
         int toPos = 1;
-        BufferedReader csvr = new BufferedReader(new FileReader("./data/stations_mod.csv"));
+        BufferedReader csvr = new BufferedReader(new FileReader(path));
         String row;
         boolean first = true;
-        HashSet<String> done = new HashSet<>();
+
         while((row = csvr.readLine()) != null)
         {
             if(first)
@@ -248,40 +236,57 @@ public class script {
             String[] data = row.split(",");
             String currSId = data[0];
             System.out.println(data[0]+" "+data[1]);
-            WebElement from = driver.findElement(By.xpath("//*[@id=\"sortable_nav\"]/li["+sidToPos.get(currSId)+"]/span"));
-//            done.add(currSId);
+            int currElePos = sidToPos.get(currSId);
+            WebElement from = driver.findElement(By.xpath("//*[@id=\"sortable_nav\"]/li["+currElePos+"]/span"));
             WebElement to = driver.findElement(By.xpath("//*[@id=\"sortable_nav\"]/li["+toPos+"]/span"));
             Actions act = new Actions(driver);
             act.dragAndDrop(from, to).build().perform();
             toPos++;
-//            for(Map.Entry e : sidToPos.entrySet())
-//            {
-////                System.out.println("updating pos...");
-//                if(!currSId.contains((CharSequence) e.getKey()))
-//                {
-//                    Integer pos = sidToPos.containsKey(e.getKey())? (Integer) e.getValue() :0;
-//                    sidToPos.put((String) e.getKey(), pos+1);
-//                }
-//            }
-            for(int i=1;i<=stats.size();i++)
+            for(Map.Entry e : sidToPos.entrySet())
             {
-                WebElement curr = driver.findElement(By.xpath("//*[@id=\"sortable_nav\"]/li["+i+"]/span"));
-                sidToPos.put(curr.getAttribute("spn"),i);
+                Integer pos = sidToPos.containsKey(e.getKey())? (Integer) e.getValue() :0;
+                if(pos<currElePos)
+                    sidToPos.put((String) e.getKey(), pos+1);
             }
 
-            try {
-                TimeUnit.SECONDS.sleep(2);
-            }
-            catch (Exception e){}
+            // In case above code portion (for loop) fails
+//            for(int i=toPos;i<=stats.size();i++)
+//            {
+//                WebElement curr = driver.findElement(By.xpath("//*[@id=\"sortable_nav\"]/li["+i+"]/span"));
+//                sidToPos.put(curr.getAttribute("spn"),i);
+//            }
+
         }
 
+        driver.findElement(By.xpath("//*[@id=\"btnSave\"]")).click();
+        driver.switchTo().alert().accept();
+    }
 
-//        WebElement from = driver.findElement(By.xpath("//*[@id=\"sortable_nav\"]/li[4]/span"));
-//        WebElement to  = driver.findElement(By.xpath("//*[@id=\"sortable_nav\"]/li[1]/span"));
-//        System.out.println(from.getAttribute("spn"));
-//        Actions act = new Actions(driver);
-//        act.dragAndDrop(from, to).build().perform();
+    public static void main(String[] args) throws IOException {
+        System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
 
-//        driver.close();
+        // login
+        driver.navigate().to("http://psd.bits-pilani.ac.in");
+        WebElement username = driver.findElement(By.id("TxtEmail"));
+        username.clear();
+        username.sendKeys(args[1]);
+        WebElement pass = driver.findElement(By.id("txtPass"));
+        pass.clear();
+        pass.sendKeys(args[2]);
+        driver.findElement(By.id("Button1")).click();
+
+        if(args[0].equals("--fetch"))
+            fetchData(driver);
+        else if(args[0].equals("--upload"))
+            uploadData(driver, args[3]);
+        else
+        {
+            System.out.println("specify function(flag)");
+            return;
+        }
+        //"4V113RHO"
+
+        driver.close();
     }
 }
